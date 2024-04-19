4a11a1500e084f73571953d1c42c6817b067d647
mehmetiyiyazici <164754796+mehmetiyiyazici@users.noreply.github.com>
2024-04-20 00:38:31 +0300

    Add files via upload

---
+++ quizgame/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace quizgame;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] sorular = ["Gökyüzü Ne Renktir? ","Türkiye'nin Başkenti Neresidir?"];
+        string[] cevaplar = ["Mavi","Ankara"];
+        string[] secenekler = ["Kırmızı|Mavi|Pembe|Mor" ,"İstanbul|Ankara|İzmir"];
+
+        int dogrucevap = 0;
+        int yanliscevap = 0;
+
+        for (int i = 0; i < sorular.Length; i++)
+        {
+            Console.WriteLine($"Soru {i + 1}: {sorular[i]}");
+            string[] secenekler2 = secenekler[i].Split('|');
+            for (int j = 0; j < secenekler2.Length; j++)
+            {
+                Console.WriteLine($"{j + 1}. {secenekler2[j]}");
+            }
+
+            Console.WriteLine("Cevabınız Nedir: ");
+            string inputcevap = Console.ReadLine();
+
+            if (inputcevap == cevaplar[i])
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Doğru");
+                Console.ResetColor();
+                dogrucevap++;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Yanlış Cevap Verdiniz");
+                Console.ResetColor();
+                yanliscevap++;
+            }
+            if (i < sorular.Length - 1)
+            {
+                Console.WriteLine("Devam etmek için bir tuşa basın...");
+                Console.ReadLine();
+                
+            }
+
+        }
+        Console.WriteLine($"Toplam doğru cevap sayısı: {dogrucevap} / {sorular.Length}");
+    }
+}  
+        
+    
---
+++ taskagıtmakas/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace taskagitmakas;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] secenekler = ["Taş", "Kağıt", "Makas"];
+
+        int kullanici = 0;
+        int bot = 0;
+
+
+        while (true)
+        {
+            Console.WriteLine("Taş,Kağıt,Makas Birini Seçiniz ");
+            string inputKullanici = Console.ReadLine();
+            Random pc = new Random();
+            int bilgisayar = pc.Next(3);
+            
+            if (inputKullanici == "Taş" && secenekler[bilgisayar] == "Kağıt")
+            {
+                bot++;
+            }
+            if (inputKullanici == "Taş" && secenekler[bilgisayar] == "Makas")
+            {
+                kullanici++;
+            }
+            if (inputKullanici == "Makas" && secenekler[bilgisayar] == "Kağıt")
+            {
+                kullanici++;
+            }
+            if (inputKullanici == "Makas" && secenekler[bilgisayar] == "Taş")
+            {
+                bot++;
+            }
+            if (inputKullanici == "Kağıt" && secenekler[bilgisayar] == "Taş")
+            {
+                kullanici++;
+            }
+            if (inputKullanici == "Kağıt" && secenekler[bilgisayar] == "Makas")
+            {
+                bot++;
+            }
+            if (inputKullanici == "Çıkış")
+            {
+                break;
+            }
+
+            Console.WriteLine("Kullanıcı " + kullanici );
+            Console.WriteLine("Bot " + bot );
+            
+            
+        }
+        
+        
+    }
+}
